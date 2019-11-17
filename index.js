5d2cc8797d2f56e456f1ff25709b8ab60d785039
robbie <robbiekruszynski@gmail.com>
2019-11-17 13:38:39 -0800

    add 404 routing and port extention

--- index.js
+++ index.js
@@ -6,14 +6,32 @@ const app = express();
 // app.put()
 // app.delete()
 
+const courses = [
+  { id: 1, name: "course 1" },
+  { id: 2, name: "course 2" },
+  { id: 3, name: "course 3" }
+];
+
 app.get("/", (req, res) => {
   res.send("hello world!!");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([0, 1, 2]);
+  res.send(courses);
 });
 
-app.listen(3010, () => {
-  console.log("listening on port 3010");
+//(req, res = route handler function)
+
+//you can have mulitple posts in a route
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("this was not found");
+  res.send(course);
+  // send a 404 error
+});
+
+//////PORT//////
+const port = process.env.PORT || 3010;
+app.listen(port, () => {
+  console.log(`listening on port ${port}...`);
 });
